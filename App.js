39d21098ada3d82e4174afe33099641e9ab59f76
Dawn Agula <2102479@ub.edu.ph>
2024-10-17 13:39:19 +0800

    Final_output

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ActivityIndIcator from './Activities/ActivityIndIcator';
+import Button from './Activities/Button';
+ import FlatList from './Activities/FlatList';
+ import Image from './Activities/Image'; 
+ import ImageBackground from './Activities/Background'; 
+ import KeyboardAvoidingView from './Activities/KeyboardAvoidingView'; 
+import Modal from './Activities/Modal';
+import Pressable from './Activities/Pressable';
+import RefreshControl from './Activities/RefreshControl';
+import ScrollView from './Activities/ScrollView';
+import SectionList from './Activities/SectionList';
+ import StatusBar from './Activities/StatusBar'; 
+import Switch from './Activities/Switch';
+ import Text from './Activities/Text'; 
+import TextInput from './Activities/TextInput';
+import TouchableHighlight from './Activities/TouchableHighlight';
+import TouchableOpacity from './Activities/TouchableOpacity';
+import TouchableWithoutFeedback from './Activities/TouchableWithoutFeedback';
+import Views from './Activities/Views';
+import VirtualizedList from './Activities/VirtualizedList';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {/*  <ActivityIndIcator/> */}
+    {/* <Button/> */}
+    {/* <flatList/> */}
+    {/* <Image/> */}
+    {/* <ImageBackground/> */}
+    {/* <KeyboardAvoidingView/> */}
+    {/* <Modal/> */}
+    {/* <Pressable/> */}
+    {/* <RefreshControl/> */}
+    {/* <ScrollView/> */}
+    {/* <SectionList/> */}
+    {/* <StatusBar/> */}
+    {/* <Switch/> */}
+    {/* <Text/> */}
+    {/* <TextInput/> */}
+    {/* <TouchableHighlight/> */}
+    {/* <TouchableOpacity/> */}
+    {/* <TouchableWithoutFeedback/> */}
+    {/* <View/> */}
+    {/*  */}
+    <VirtualizedList/>
+
+    
+    
     </View>
   );
 }
